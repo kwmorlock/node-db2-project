10f3e502e813d6382d8173e17a75d4903b146e8a
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-13 01:06:01 -0400

    first get

--- router/carsRouter.js
+++ router/carsRouter.js
@@ -1,21 +1,18 @@
 const express = require("express");
 
+const db = require("../data/dbConnection.js");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
     db("cars") //name of table
       .then(cars => {
-        res.json(cars;
+        res.json(cars);
       })
       .catch(err => {
         res.status(500).json({ message: "Failed to retrieve fruits" });
       });
   });
-
-
-
-
-
 
 module.exports = router;
 
