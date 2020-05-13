0d925dd67e437b2b005eaf5ffa003483c9d7b485
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-13 01:07:45 -0400

    second get

--- router/carsRouter.js
+++ router/carsRouter.js
@@ -10,9 +10,26 @@ router.get("/", (req, res) => {
         res.json(cars);
       })
       .catch(err => {
-        res.status(500).json({ message: "Failed to retrieve fruits" });
+        res.status(500).json({ message: "Failed to retrieve vroom" });
       });
   });
 
+  router.get("/:id", (req, res) => {
+    const { id } = req.params;
+  
+    db("cars")
+      .where({ id })
+      .first()
+      .then(car => {
+        res.json(car);
+      })
+      .catch(err => {
+        res.status(500).json({ message: "Failed to retrieve vroom" });
+      });
+  });
+
+
 module.exports = router;
+
+
 
